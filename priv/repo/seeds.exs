32d4ee8f199c77d600d0845df25836b3f2d65257
Ryan Wold <>
2023-05-03 20:05:55 -0700

    seed 100 nodes and 100 links at random
    * run using `mix run priv/repo/seeds.exs`

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,21 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+Adaptics.Repo.delete_all Adaptics.Visual.Node
+Adaptics.Repo.delete_all Adaptics.Visual.Link
+
+(1..100) |> Enum.each(fn i ->
+  Adaptics.Repo.insert! %Adaptics.Visual.Node{
+    name: "Name #{i}",
+    description: "Description #{i}"
+  }
+end)
+
+(1..100) |> Enum.each(fn i ->
+  Adaptics.Repo.insert! %Adaptics.Visual.Link{
+    name: "Name #{i}",
+    description: "Description #{i}",
+    from_id: Enum.random(1..100),
+    to_id: Enum.random(1..100)
+  }
+end)
